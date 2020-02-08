9cb14109288b01a37de1ae55242d23b9b27b202f
Basruhe <sb.ruhe@gmail.com>
2020-02-08 16:29:08 +0100

    Final assignment1

--- messages-api.js
+++ messages-api.js
@@ -1,7 +1,7 @@
 // assignment 1
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3001;
 const bodyParser = require("body-parser");
 
 // To test with httpie: http :3000/messages
@@ -9,13 +9,44 @@ app.get("/messages", (request, response) =>
   response.send("hello from messages-api.js, endpoint /messages!")
 );
 
-// adding POST
+//Count validation
+let requestCount = 0;
+const requestCountMiddleware = (request, response, next) => {
+  requestCount += 1;
+  console.log("Max requests validation called");
+  if (requestCount >= 5) {
+    console.log(`Current number of requests: ${requestCount}`);
+    response.status(429).end();
+  } else {
+    console.log(`Current number of requests: ${requestCount}`);
+    next();
+  }
+};
 
+//Faulty input validation
+let faultyInputValidation = (request, response, next) => {
+  console.log("Faulty input validation called");
+  if (
+    request.body.text ||
+    request.body.text.length == "" ||
+    request.body.text.length == 0
+  ) {
+    console.log("Error: your input is either too short or of the wrong type");
+    response.status(429).end();
+  } else {
+    next();
+  }
+};
+
+// application-level middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(requestCountMiddleware);
+// app.use(faultyInputValidation);
 
+// adding POST
 app.post("/comments", (request, response, next) => {
-  console.log("request input received and parsed into the following:");
+  console.log("Request input received and parsed into the following:");
   console.log(request.body);
   response.json({
     message: "Your POST request is received!"
@@ -26,27 +57,6 @@ app.listen(port, () =>
   console.log(`express server active, listening to port ${port} `)
 );
 
-// 1. Create a new JS file named `messages-api.js`.
-// 1. Create an Express app in that file.
-// The app should listen for requests on port `3000`.
-// Make sure you add the required dependency.
-// 1. Add a single endpoint to the app responds to `POST` requests to the `/messages` URI.
-// 1. When a request is sent to the endpoint, it should log the `text` property of the body to the console, and it should respond with a JSON object, for example:
-
-//    ```javascript
-//    {
-//       "message": "This is the message that was sent"
-//    }
-//    ```
-
-//    In order to parse the JSON body of the request, you will need to add the middleware for it.
-// Make sure you add the required dependency.
-// 1. Perform the following validation: if the body does NOT have a `text` property or the string is empty, then send a "Bad Request" HTTP status code to the client.
-// 1. The API should only log the message five times.
-
-//    After receiving five messages, sixth request should be sent a response that indicates the HTTP status for "Too Many Requests".
-
-//    Make sure the correct HTTP status code is sent (Google it if you haven't seen this status message before).
-
-//    Although there are libraries to implement such limits, do **not** use them! Implement the logic yourself.
-// 1. Put the message limit logic from the previous step into a middleware function. It should behave the same.
+//TODO:
+// change faulty input validation. Change logic? or add at router level?
+// clean consoles and comments
